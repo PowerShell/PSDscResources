2d3d4595bbcd080f8d4f9ff9dddb00a56aafa7fc
Mariah Breakey <mbreakey@microsoft.com>
2016-12-15 16:22:36 -0800

    breaking up path for importing helper

--- DscResources/MSFT_GroupResource/MSFT_GroupResource.psm1
+++ DscResources/MSFT_GroupResource/MSFT_GroupResource.psm1
@@ -63,8 +63,11 @@
 $errorActionPreference = 'Stop'
 Set-StrictMode -Version 'Latest'
 
-Import-Module -Name (Join-Path -Path (Split-Path -Path $PSScriptRoot -Parent) `
-                               -ChildPath 'CommonResourceHelper.psm1')
+# Import CommonResourceHelper for Test-IsNanoServer
+$script:dscResourcesFolderFilePath = Split-Path $PSScriptRoot -Parent
+$script:commonResourceHelperFilePath = Join-Path -Path $script:dscResourcesFolderFilePath -ChildPath 'CommonResourceHelper.psm1'
+Import-Module -Name $script:commonResourceHelperFilePath
+
 $script:localizedData = Get-LocalizedData -ResourceName 'MSFT_GroupResource'
 
 if (-not (Test-IsNanoServer))
