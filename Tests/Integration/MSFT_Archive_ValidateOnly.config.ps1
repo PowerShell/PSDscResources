1f0238b206a35fb5c4587819b5ccaca70cd920e9
Katie Keim <kakeim@microsoft.com>
2017-02-22 15:43:09 -0800

    Add Archive resource

---
+++ Tests/Integration/MSFT_Archive_ValidateOnly.config.ps1
@@ -0,0 +1,44 @@
+﻿param
+(
+    [Parameter(Mandatory = $true)]
+    [String]
+    $ConfigurationName
+)
+
+Configuration $ConfigurationName
+{
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [String]
+        $Path,
+
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [String]
+        $Destination,
+
+        [Parameter()]
+        [ValidateSet('Present', 'Absent')]
+        [String]
+        $Ensure = 'Present',
+
+        [Parameter()]
+        [Boolean]
+        $Validate = $false
+    )
+
+    Import-DscResource -ModuleName 'PSDscResources'
+
+    Node localhost
+    {
+        Archive Archive1
+        {
+            Path = $Path
+            Destination = $Destination
+            Ensure = $Ensure
+            Validate = $Validate
+        }
+    }
+}
