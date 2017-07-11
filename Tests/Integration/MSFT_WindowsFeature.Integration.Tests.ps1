e3a3535142de499012e2c2da2a3549a7f62bdb95
Norberto Arrieta <narrieta@users.noreply.github.com>
2017-07-11 15:11:58 -0700

    Fix versionn check (#79)

--- Tests/Integration/MSFT_WindowsFeature.Integration.Tests.ps1
+++ Tests/Integration/MSFT_WindowsFeature.Integration.Tests.ps1
@@ -11,7 +11,7 @@
 [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidUsingConvertToSecureStringWithPlainText', '')]
 param ()
 
-if ($PSVersionTable.PSVersion.Major -lt 5 -or $PSVersionTable.PSVersion.Minor -lt 1)
+if ($PSVersionTable.PSVersion -lt [Version] '5.1')
 {
     Write-Warning -Message 'Cannot run PSDscResources integration tests on PowerShell versions lower than 5.1'
     return
