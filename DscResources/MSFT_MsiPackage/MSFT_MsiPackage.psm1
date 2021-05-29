c4481ab0d4a116bfffa428e35eb743a6d494c849
Daniel Scott-Raynsford <plagueho@gmail.com>
2021-05-29 21:06:26 +1200

    Fix style errors found by meta tests

--- DscResources/MSFT_MsiPackage/MSFT_MsiPackage.psm1
+++ DscResources/MSFT_MsiPackage/MSFT_MsiPackage.psm1
@@ -1,7 +1,8 @@
 # Suppress Global Vars PSSA Error because $global:DSCMachineStatus must be allowed
 [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidGlobalVars', '')]
 [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseShouldProcessForStateChangingFunctions', '')]
-param()
+
+param ()
 
 $errorActionPreference = 'Stop'
 Set-StrictMode -Version 'Latest'
